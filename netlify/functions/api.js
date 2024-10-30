d97f1c234d10f37df9b1c4303d00f900df128294
Selvaraj <selvarajp9311@gmail.com>
2024-10-30 16:28:24 +0530

    netfiley

---
+++ netlify/functions/api.js
@@ -0,0 +1,8 @@
+// netlify/functions/api.js
+exports.handler = async (event, context) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello from Netlify!" })
+    };
+  };
+  
